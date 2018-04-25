049e79bffb9b3f68f132de70cf33660dfb3a0d5d
root <user3610@protonmail.com>
2018-04-25 21:19:14 +0300

    minor

--- src/parser.py
+++ src/parser.py
@@ -7,10 +7,10 @@ def parser(logs):
     # Parses the text and then outputs it to output folder
     file = open(str(logs), 'r')
     check = []
-    print "Reading {}".format(logs)
+    print ("Reading {}".format(logs))
     filename = "./output/" + datetime.datetime.now().strftime("%y-%m-%d-%H-%M-%S") + ".txt"
     output = open(str(filename), 'w+')
-    print "Writing {} to output".format(filename)
+    print ("Writing {} to output".format(filename))
     for line in file.readlines():
         if not re.findall(r'\d{2,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line) or re.findall(r'\d{2,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line) in check:
             continue
