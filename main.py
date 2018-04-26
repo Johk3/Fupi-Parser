04f9fea601a8d40c0e062b55464fba4a0d5f4f9d
root <user3610@protonmail.com>
2018-04-26 15:47:38 +0300

    Added classes

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import os
+import datetime
 
 from src import parser
 
 files = os.listdir("logs")
+filename = "./output/" + datetime.datetime.now().strftime("%y-%m-%d-%H-%M-%S") + ".txt"
 
 for file in files:
     if file == "readme.txt":
         continue
     else:
-        parser.parser("./logs/" + file)
+        configured = parser.Parser("./logs/" + file, filename)
+        configured.mainip()
 
 
--- src/parser.py
+++ src/parser.py
@@ -1,22 +1,36 @@
 # This parses the data and returns only the goods.
 import re
-import datetime
 
 
-def parser(logs):
+class Parser(object):
+
+    def __init__(self, logs, filename):
+        self.logs = logs
+        self.filename = filename
+
     # Parses the text and then outputs it to output folder
-    file = open(str(logs), 'r')
-    check = []
-    print("Reading {}".format(logs))
-    filename = "./output/" + datetime.datetime.now().strftime("%y-%m-%d-%H-%M-%S") + ".txt"
-    output = open(str(filename), 'w+')
-    print("Writing {} to output".format(filename))
-    for line in file.readlines():
-        if not re.findall(r'\d{2,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line) or re.findall(r'\d{2,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line) in check:
-            continue
-        else:
-            check.append(re.findall(r'\d{2,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line))
-            output.write("\n" + "".join(re.findall(r'\d{2,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line)))
+    def mainip(self):
+        file = open(str(self.logs), 'r')
+        check = []
+        out = []
+        ip_address = r'\d{2,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+        print("Reading {}".format(self.logs))
+        output = open(str(self.filename), 'w+')
+        print("Writing {} to output".format(self.filename))
+        i = 1
+        for line in file.readlines():
+            if "++++++++++++++++++BEGIN NEW DASH TEST!!!++++++++++++++" in line:
+                print("++++++++++++++TIMESTAMP NUMBER {} ++++++++++++++".format(i))
+                i+=1
+
+            if not re.findall(ip_address, line) or re.findall(ip_address, line) in check:
+                continue
+
+            else:
+                check.append(re.findall(ip_address, line))
+                out.append(re.findall(ip_address, line))
+        print("Done")
+        output.write(str(out))
 
 
 
